50e06dc57c325941ac55caacfd28e805f84c2e0c
DennisSab <dennis.sabanis2002@gmail.com>
2024-09-10 00:24:32 +0300

    Update Version2Fast.py

--- Version2Fast.py
+++ Version2Fast.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 
 # Data for the diagram
 cores = [4, 8, 16, 32, 64]
-times = [64.101, 56.759, 45.317, 38.016, 25.592]
+times = [93.687, 74.031, 52.788, 45.526, 20.225]
 
 # Create the plot
 plt.figure(figsize=(8, 6))
 plt.plot(cores, times, marker='o', linestyle='-', color='b')
 
 # Add titles and labels
-plt.title('Version2Fast-Execution Times vs Number of Cores')
+plt.title('Version2InsertOnly-Execution Times vs Number of Cores')
 plt.xlabel('Number of Cores')
 plt.ylabel('Execution Time (ms)')
 
